ff981509c1f4347f6c9448cf89000ff0a8988bbb
Jim Laney <jlaney@uw.edu>
2017-03-14 10:47:01 -0700

    add files for utilities and exceptions

---
+++ uw_nws/exceptions.py
@@ -0,0 +1,10 @@
+
+
+class InvalidUUID(Exception):
+    """Exception for invalid UUID."""
+    pass
+
+
+class InvalidEndpointProtocol(Exception):
+    """Exception for invalid endpoint protocol."""
+    pass
---
+++ uw_nws/utilities.py
@@ -0,0 +1,4 @@
+from commonconf import override_settings
+
+
+fdao_nws_override = override_settings(RESTCLIENTS_NWS_DAO_CLASS='Mock')
